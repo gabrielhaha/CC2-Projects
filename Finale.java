80485bd11833555cc8ee2e351fdaf471b0ffb3c1
gabrielhaha <44487021+gabrielhaha@users.noreply.github.com>
2018-11-23 08:43:44 +0800

    Add files via upload

--- Finale.java
+++ Finale.java
@@ -2,50 +2,71 @@ package finale;
 import java.util.Scanner;
 public class Finale {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        
-        System.out.println("What do you want to get?  Factorial/Fibonacci? ");
-        String ab = sc.nextLine();
-        System.out.println("Enter desired size of array");
-        int cd = sc.nextInt();
-        int[] array = new int[cd];
-        System.out.println("Enter Elements: ");
-        for(int i = 0; i< array.length; i++){
-            int ef = sc.nextInt();
-            array[i] = ef;
-        }
-        for(int i = 0; i < array.length; i++){
-            System.out.print("f(" + array[i] + ") \t");
-        }
-        System.out.println("");
-        if(ab.equalsIgnoreCase("Factorial")){
-            for(int i = 0; i < array.length; i++){
-            System.out.print(factorial(array[i]) + "\t");
-            }
-        }else if (ab.equalsIgnoreCase("Fibonacci")){
-            for(int i = 0; i < array.length; i++){
-            System.out.print(fibonacci(array[i]) + "\t");
-            }
-        } else{
-            System.out.println("Invalid input");
-        }
-    }
-    public static int fibonacci(int x){
-        if(x <=1){
-            return x;
-        }else{
-            return fibonacci(x-1)+fibonacci(x-2);
-        }
-    }
-    public static int factorial(int x){
-        int i = 1;
-        int factorial = 1;
-        while(i <= x){
-            factorial *= i;
-            i++;
-        }
-        return factorial;
-    }
-}
+    static String input;
+    static Scanner sc =new Scanner (System.in);
+    static Scanner fact= new Scanner (System.in);
 
+    public static void main(String[] args) {
+        System.out.println(inputmethod()); 
+    }
+    
+    public static String inputmethod(){
+        System.out.println("Enter a word: ");
+        input=sc.next();
+        if (input.equalsIgnoreCase("factorial")){
+                System.out.print("Enter the size of array: ");
+                int y = fact.nextInt();
+                int [] factar= new int [y];
+                
+                System.out.println("Enter "+y+" numbers: ");
+                for (int i=0; i<factar.length;i++) 
+                {
+                factar [i] = fact.nextInt();
+                System.out.print("");
+                }
+                
+                //This is for the Fibonacci counter (e.g. f(0),f(1) and so on and so forth)
+
+                for (int b=0; b<y;b++){System.out.print("f ("+factar[b]+")\t");}
+                System.out.println();
+                
+                 for (int column=0;column<y;column++) { System.out.print(factorial(factar[column])+"\t");}
+        }
+        else if (input.equalsIgnoreCase("fibonacci")){
+                Scanner fib= new Scanner (System.in);
+                System.out.print("Enter a number: ");
+                int a=fib.nextInt();
+
+                //This is for the Fibonacci counter (e.g. f(0),f(1) and so on and so forth)
+
+                for (int b=0; b<a+1;b++){System.out.print("f ("+b+")\t");}
+                System.out.println();
+
+                //This is for the rows and columns
+                for (int row = 0; row < a+1; row++){
+                    for (int column=0;column<row+1;column++){ System.out.print(fact(column)+"\t");}
+                    System.out.println(); }
+        }
+        else {return inputmethod();}
+        return input;
+    }
+    
+    
+    
+   //This method is for the factorial
+    public static int factorial (int n){
+    if (n<=1){
+        return 1;
+    }else {
+        return n*factorial(n-1);
+    }}  
+     
+    //This method is for the fibonacci
+    public static int fact (int m){
+    if (m<=1)
+        { return m ;} 
+        else 
+        { return fact (m-1)+ fact (m-2);}
+        
+    }    
+}
